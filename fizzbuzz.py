438d64f0c20283ec7fc90b179cfccf4f7d71fccd
seanmoserr <seanmoserr@gmail.com>
2024-03-07 16:55:21 -0600

    This version of the program prints all numbers, 1-100 and outputs the corresponding messages for the iteration of the index. However, the messages print out purely on an if basis, so furhter iterations are needed to replace console output based on the index iteration.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,3 +3,11 @@
 #divisible by 3, 'buzz' if divisible by 5, 
 #and 'fizzbuzz' if divisible by both
 
+for i in range(101):
+    print(i)
+    if(i % 3 == 0):
+        print("Fizz")
+    if(i % 5 == 0):
+        print("Buzz")
+    if(i % 3 and i % 5 == 0):
+        print("FizzBuzz")
