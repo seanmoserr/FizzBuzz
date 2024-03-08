68a0219a5360e745d4dcaeb9ce8b550fea614624
seanmoserr <seanmoserr@gmail.com>
2024-03-07 18:14:40 -0600

    Fixed works

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,3 +2,12 @@
 #writing 'fizz' if the current index is 
 #divisible by 3, 'buzz' if divisible by 5, 
 #and 'fizzbuzz' if divisible by both
+
+for i in range(101):
+    print(i)
+    if(i % 3 == 0):
+        print("Fizz")
+    if(i % 5 == 0):
+        print("Buzz")
+    if(i % 3 and i % 5 == 0):
+        print("FizzBuzz")
